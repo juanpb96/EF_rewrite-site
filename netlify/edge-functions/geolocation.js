7faf74d2953e14d7008a56c8d123093cfeeba02f
MasterDev <juanpb.96@gmail.com>
2025-10-01 01:14:28 -0500

    Test with redirects for assets

--- netlify/edge-functions/geolocation.js
+++ netlify/edge-functions/geolocation.js
@@ -1,9 +1,9 @@
 import { GEO_HEADERS } from "./utils/constants.js";
 
-const externalUrl = "https://juanbonilla-fem-weather-app.netlify.app";
+const externalUrl = "https://juan-edge-function-test.netlify.app/test/";
 
 export default async (request, context) => {
-  const { country, city, latitude, longitude } = context.geo;
+  // const { country, city, latitude, longitude } = context.geo;
   // request.headers.set(GEO_HEADERS.country, country?.name ?? "");
   // request.headers.set(GEO_HEADERS.city, city ?? "");
   // request.headers.set(GEO_HEADERS.latitude, String(latitude ?? 0));
@@ -13,25 +13,21 @@ export default async (request, context) => {
   try {
     const response = await fetch(externalUrl);
 
-    const headers = new Headers(response.headers);
-    headers.set(GEO_HEADERS.country, country?.name ?? "");
-    headers.set(GEO_HEADERS.city, city ?? "");
-    headers.set(GEO_HEADERS.latitude, String(latitude ?? 0));
-    headers.set(GEO_HEADERS.longitude, String(longitude ?? 0));
-    return new Response(response.body, { status: response.status, headers });
+    // const headers = new Headers(response.headers);
+    // headers.set(GEO_HEADERS.country, country?.name ?? "");
+    // headers.set(GEO_HEADERS.city, city ?? "");
+    // headers.set(GEO_HEADERS.latitude, String(latitude ?? 0));
+    // headers.set(GEO_HEADERS.longitude, String(longitude ?? 0));
+    // return new Response(response.body, { status: response.status, headers });
     // You can modify headers, status, etc., if needed
     // For example, to set a specific Content-Type:
     // const headers = new Headers(response.headers);
     // headers.set("Content-Type", "text/html");
     // return new Response(response.body, { status: response.status, headers });
 
-    // return response; // Return the fetched response directly
+    return response; // Return the fetched response directly
   } catch (error) {
     context.log("Error fetching external resource:", error);
     return new Response("Error fetching external content", { status: 500 });
   }
-};
-
-export const config = {
-  path: "/test",
 };
